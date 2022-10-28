39ad559d4aacd35e9207adb462bc904c4f9a8a86
rkota(Rosaire Kota) <rosairekota@gmail.com>
2022-10-28 10:47:19 +0100

    feat: add mutations

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,38 @@
+const users = [
+  {id:1, name:"kota",age:10, connected: true}
+]
+for (let i = 0; i < 100; i++) {
+  users.push({id:i+1, name:"kota",age:10+i, connected: true})
+  
+}
 const resolvers ={
   Query:{
     users:()=>{
-        return "ok"
-    }
+        return users
+    },
+    user:(age =0)=>{
+      return users.find((item)=>item.age===+age)
+  }
+  },
+
+  Mutation:{
+     createUser: (parent, user)=>{
+      const newUser = user
+      users.push(user);
+      return newUser
+     },
+
+     updateUser: (parent, user)=>{
+       const usersFiltered = users.filter((item)=>item.id!==+user.id)
+      const newUser = {...user}
+      usersFiltered.push(newUser);
+      return usersFiltered
+     },
+     
+     deleteUser: (parent, id)=>{
+      const usersFiltered = users.filter((item)=>item.id!==+id)
+      return usersFiltered
+     }
   }
 }
 module.exports = {
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,16 +1,21 @@
 const {gql} = require('apollo-server-express')
 const typeDefs  = gql`
 type User {
-    name: String!,
+    id: Int!
+    name: String!
     age: Int!
     connected: Boolean
 }
 
 type Query{
     users:[User!]
-    user: User
+    user(age:Int): User
 }
-
+type Mutation {
+    createUser( id: Int!, name: String!, age: Int!, connected: Boolean): User!
+    updateUser( id: Int!, name: String!, age: Int!, connected: Boolean): [User!]
+    deleteUser( id: Int!): [User!]
+}
 `
 module.exports = {
     typeDefs
